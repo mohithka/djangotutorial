486a34ea55d8f8f7ff4144009931f53aef61a84a
mohithka <mohithk0018@gmail.com>
2025-09-25 12:32:07 +0530

    Your commit message

--- polls/admin.py
+++ polls/admin.py
@@ -1,18 +1,7 @@
 from django.contrib import admin
 from .models import Question, Choice
 
-class ChoiceInline(admin.TabularInline):   # Inline choices inside question
-    model = Choice
-    extra = 3   # show 3 empty choices by default
 
-class QuestionAdmin(admin.ModelAdmin):
-    fieldsets = [
-        (None, {'fields': ['question_text']}),
-        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
-    ]
-    inlines = [ChoiceInline]  # connect choices here
-    list_display = ('question_text', 'pub_date')
-    list_filter = ['pub_date']
-    search_fields = ['question_text']
 
-admin.site.register(Question, QuestionAdmin)
+admin.site.register(Question)
+admin.site.register(Choice)
